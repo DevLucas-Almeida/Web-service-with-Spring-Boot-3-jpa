f45a78dd413d664f53e15e02b564b9ae49b0cd48
Lucas Almeida <lucasalmeida.1407@outlook.com>
2023-01-12 16:43:23 -0300

    Payment, one-to-one association

--- src/main/java/com/projetoSpring/courseSpring/config/TestConfig.java
+++ src/main/java/com/projetoSpring/courseSpring/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.projetoSpring.courseSpring.enteties.Category;
 import com.projetoSpring.courseSpring.enteties.Order;
 import com.projetoSpring.courseSpring.enteties.OrderItem;
+import com.projetoSpring.courseSpring.enteties.Payment;
 import com.projetoSpring.courseSpring.enteties.Product;
 import com.projetoSpring.courseSpring.enteties.User;
 import com.projetoSpring.courseSpring.enteties.enums.OrderStatus;
@@ -77,5 +78,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
