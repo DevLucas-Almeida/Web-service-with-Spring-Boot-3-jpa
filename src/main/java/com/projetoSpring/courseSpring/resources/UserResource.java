74dee2400bc7b251653d1b8652d621b8939320da
Lucas Almeida <lucasalmeida.1407@outlook.com>
2023-01-13 10:51:12 -0300

    User delete

--- src/main/java/com/projetoSpring/courseSpring/resources/UserResource.java
+++ src/main/java/com/projetoSpring/courseSpring/resources/UserResource.java
@@ -1,18 +1,17 @@
 package com.projetoSpring.courseSpring.resources;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetoSpring.courseSpring.enteties.User;
 import com.projetoSpring.courseSpring.services.UserService;
@@ -37,9 +36,15 @@ public class UserResource {
 	}
 	
 	@PostMapping
-	  public ResponseEntity<User> insertUser(@RequestBody User user){
-	    User response = service.insert(user);
-	    return new ResponseEntity<User>(response, HttpStatus.CREATED);
+	public ResponseEntity<User> insert(@RequestBody User user){
+	   User response = service.insert(user);
+	   return new ResponseEntity<User>(response, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
